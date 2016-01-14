6db3688cbe5628e3881a869a596c0ce3c8687994
Andy <brownie3003@gmail.com>
2016-01-14 16:32:05 +0000

    Add a project route

--- app/router.js
+++ app/router.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function () {
+    this.route('projects');
 });
 
 export default Router;
