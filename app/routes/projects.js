5c7c94ab36816000fa1a795fc2b23c876058f006
Andy <brownie3003@gmail.com>
2016-01-15 15:31:54 +0000

    Section V: Actions
    Add a destroy project action

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('project');
+    },
+    actions: {
+        deleteProject(project) {
+            return project.destroyRecord();
+        }
     }
 });
